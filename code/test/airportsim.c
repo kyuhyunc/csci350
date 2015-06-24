f976ac7f53cd7599522880811e87499e3c5a5d1e
Jerry <jerryzhang93@gmail.com>
2015-06-23 17:21:19 -0700

    included airportsim file

---
+++ code/test/airportsim.c
@@ -0,0 +1,23 @@
+/*	airportsim.c
+	Airport Simulation
+ */
+
+#include "syscall.h"
+
+struct random {
+	int a;
+	int b;
+	int c;
+};
+
+int main() {
+	struct random Try1;
+	Try1.a = 1;
+	Try1.b = 2;
+	Try1.c = 3;
+
+	Printf0("Write all airport code in this file!\n", sizeof("Write all airport code in this file!\n"));
+
+	Exit(0);
+
+}
