a2a41353a3d641543135ee80520b9abb4870bdaa
Greg Berger <grberger@usc.edu>
2015-07-09 20:31:43 -0700

    implemented deletelock test

--- code/test/clientsim.c
+++ code/test/clientsim.c
@@ -5,35 +5,57 @@
 *
 */
 #include "syscall.h"
+#define true 1
+#define false 0
+typedef char bool;
+
+/*
+*	Test 1 - CreateLock test
+*/
+bool test0_createLock() {
+	int lock0, lock1, lock2;
+
+	Printf0("Running test0_createLock\n", sizeof("Running test0_createLock\n"));
+
+	lock0 = CreateLock("lock0", sizeof("lock0"));
+	lock1 = CreateLock("lock1", sizeof("lock1"));
+	lock2 = CreateLock("lock0", sizeof("lock0")); /* should return same index as lock0 */
+
+	if(lock0 == 0 && lock1 == 1 && lock2 == 0) {
+		Printf0("test0_createLock passed!\n", sizeof("test0_createLock passed!\n"));
+		return true;
+	}else{
+		Printf0("test0_createLock failed!\n", sizeof("test0_createLock failed!\n"));
+		return false;
+	}
+}
+
+/*
+*	Test 1 - DeleteLock test
+*/
+bool test1_deleteLock() {
+	int lock0, lock1, destroyResult;
+	
+	Printf0("Running test1_deleteLock\n", sizeof("Running test1_deleteLock\n"));
+
+	lock0 = CreateLock("lock0", sizeof("lock0"));
+	destroyResult = DestroyLock(lock0);
+
+	lock1 = CreateLock("lock0", sizeof("lock0")); /* should not return same address */
+
+	if(destroyResult == lock0 && lock0 != lock1) {
+		Printf0("test1_deleteLock passed!\n", sizeof("test1_deleteLock passed!\n"));
+		return true;
+	} else {
+		Printf0("test1_deleteLock failed!\n", sizeof("test1_deleteLock failed!\n"));
+		return false;
+	}
+}
 
 /*
 *	"main"
 */
-int test1_1, test1_2, test1_3;
-
-void test1();
-
 int main() {
-	Fork(test1, "test1", sizeof("test1"));
-
-}
-
-void test1() {
-
-	Printf0("Test1: CreateLock index test\n", sizeof("Test1: CreateLock index test\n"));
-
-	test1_1 = CreateLock("lock1", sizeof("lock1"));
-	test1_2 = CreateLock("lock2", sizeof("lock2"));
-	test1_3 = CreateLock("lock1", sizeof("lock1"));
-
-	Printf0("About to DestroyLock", sizeof("About to DestroyLock"));
-	DestroyLock(test1_3);
-
-	if(test1_1 == test1_3) {
-		Printf0("Test1 passed!\n", sizeof("Test1 passed!\n"));
-	}else{
-		Printf0("Test1 failed!\n", sizeof("Test1 failed!\n"));
-	}
-
-	Exit(0);
+	test0_createLock();
+	test1_deleteLock();
 }
