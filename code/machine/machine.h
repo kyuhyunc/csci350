ee67934b2ac8fb594cb49b47d007f28e512459f3
Jerry <jerryzhang93@gmail.com>
2015-07-17 01:04:50 -0700

    Infinite memory, and revert network Makefile to run with pageTable instead of TLB

--- code/machine/machine.h
+++ code/machine/machine.h
@@ -34,7 +34,7 @@ using namespace std;
 					// the disk sector size, for
 					// simplicity
 
-#define NumPhysPages    32
+#define NumPhysPages    5000
 // NumPhysPages originally was 32
 #define MemorySize 	(NumPhysPages * PageSize)
 #define TLBSize		4		// if there is a TLB, make it small
