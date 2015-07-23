95b2eec83592ca1b5ef2f4cc410c8002a586105c
Greg Berger <grberger@usc.edu>
2015-07-22 19:30:56 -0700

    Trying something new again.

--- code/test/create.h
+++ code/test/create.h
@@ -399,14 +399,6 @@ void createSecurityInspectors() {
 					10
 				);
 		Printf1("%d\n", sizeof("%d\n"), si);
-		Printf0(
-				concatNumToString(
-						"SI",
-						sizeof("SI"),
-						i
-					),
-					sizeof("SI") + 3
-			);
 		/* Add SI to array of SIs */
 		SetMV(securityInspectors, i, si);
 		
