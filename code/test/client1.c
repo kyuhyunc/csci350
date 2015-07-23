001d09525f7be33f60ff0d2e6c839f864fff3adb
Greg Berger <grberger@usc.edu>
2015-07-23 13:24:57 -0700

    Maybe fixed it

--- code/test/client1.c
+++ code/test/client1.c
@@ -5,11 +5,11 @@ extern void doCreates();
 int main(){
 	int i;
 	char* test;
-	char buf[30];
+	/*char buf[30];*/
 
-	for (i = 0; i < 30; ++i) {
+/*	for (i = 0; i < 30; ++i) {
 		buf[i] = '3';
-	}
+	}*/
 
 	for (i = 0; i < NUM_PASSENGERS; ++i) {
 		/*Exec("../test/passenger", sizeof("../test/passenger"));*/
@@ -21,14 +21,14 @@ int main(){
 	*		Okay to Delete, once it's time to start simulating the airport
 	*/
 	/*doCreates();*/
-	test = 	(char*) (ConcatNumToString(
+/*	test = 	(char*) (ConcatNumToString(
 							"Uhh",
 							sizeof("Uhh"),
 							30
 						)
-					);
+					);*/
 	Printf0(
-			buf,
+			test,
 			30
 		);
 }
