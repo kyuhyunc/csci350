fd474777262e29feb331defbb2534728d5f3eb0f
Greg Berger <grberger@usc.edu>
2015-07-22 17:21:34 -0700

    Put print statement in doCreates to test extern

--- code/test/client1.c
+++ code/test/client1.c
@@ -1,8 +1,17 @@
 #include "create.h"
 
+extern void doCreates();
+
 int main(){
 	int i;
 	for (i = 0; i < NUM_PASSENGERS; ++i) {
-		Exec("../test/passenger", sizeof("../test/passenger"));
+		/*Exec("../test/passenger", sizeof("../test/passenger"));*/
 	}
+
+
+	/*
+	*	Testing 
+	*		Okay to Delete, once it's time to start simulating the airport
+	*/
+	doCreates();
 }
