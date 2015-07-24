3e818a1d07ed6f3abed54006e5838e9fdabc3f2f
Greg Berger <grberger@usc.edu>
2015-07-24 11:21:20 -0700

    Only create one liaison and one passenger

--- code/test/client2.c
+++ code/test/client2.c
@@ -3,12 +3,17 @@
 int main(){
 	int i, j;
 
-	for (i = 0; i < NUM_LIASONS; ++i) {
+	/*for (i = 0; i < NUM_LIASONS; ++i) {
+		Exec("../test/liaison", sizeof("../test/liaison"));
+	}*/
+
+	for (i = 0; i < 1; ++i) {
 		Exec("../test/liaison", sizeof("../test/liaison"));
 	}
-	for (i = 0; i < NUM_AIRLINES; ++i) {
+
+	/*for (i = 0; i < NUM_AIRLINES; ++i) {
 		for (j = 0; j < NUM_CIS_PER_AIRLINE; ++j) {
 			Exec("../test/checkinstaff", sizeof("../test/checkinstaff"));
 		}
-	}
+	}*/
 }
