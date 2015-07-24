5ec71338aa09ba46618994dc79ec1f488b7b965e
Richard Fu <fur@usc.edu>
2015-07-24 12:42:24 -0700

    convert cis

--- code/test/checkinstaff.c
+++ code/test/checkinstaff.c
@@ -21,76 +21,76 @@ int concat2Num(int i, int j) {
 
 
 void startCheckInStaff() {
-#define myAirline Airlines[_myAirline]
-#define my myAirline._cis[_myIndex]
-#define passenger Passengers[my._currentPassenger]
+#define myAirline GetMV(airlines, _myAirline);
+#define my GetMV(GetMV(GetMV(airlines, _myAirline), AirlineCIS), _myIndex);
+#define passenger GetMV(passengers, GetMV(my, CISCurrentPassenger))
 	/* Claim my CIS */
 	int _myAirline;
 	int _myIndex; /* ID for currentThread */
     Acquire(GlobalDataLock);
-	_myIndex = NumActiveCIS % NUM_CIS_PER_AIRLINE;
-	_myAirline = NumActiveCIS / NUM_CIS_PER_AIRLINE;
-    NumActiveCIS++;
+	_myIndex = GetMV(NumActiveCIS, 0) % NUM_CIS_PER_AIRLINE;
+	_myAirline = GetMV(NumActiveCIS, 0) / NUM_CIS_PER_AIRLINE;
+    IncrementMV(NumActiveCIS, 0);
     Release(GlobalDataLock);
 
     while (true) {
 		/* Check lines */
-		Acquire(myAirline._lock);
-		if (my._lineSize == 0 && queue_empty(&myAirline._execQueue)) {
+		Acquire(GetMV(myAirline, AirlineLock);
+		if (GetMV(my, CISLIneSize) == 0 && queue_empty(GetMV(myAirline, AirlineExecQueue))) {
 			/*Printf0("1\n", sizeof("1\n"));*/
-			my._state = ONBREAK;
+			SetMV(my, CISState, ONBREAK);
 			/* 'Clock Out' for Break */
-			myAirline._numOnBreakCIS++;
+			IncrementMV(myAirline, AirlineNumOnBreakCIS);
 /*Printf1("Cis %d going to sleep\n", sizeof("Cis %d going to sleep\n"), _myIndex);*/
-			Wait(myAirline._lock, my._commCV); /* Wait on Manager */ /* TODO - make sure okay to wait on aiport lock... maybe better? */
+			Wait(GetMV(myAirline, AirlineLock), GetMV(my, CISCommCV)); /* Wait on Manager */ /* TODO - make sure okay to wait on aiport lock... maybe better? */
 /*Printf1("Cis %d woke up by manager\n", sizeof("Cis %d woke up by manager\n"), _myIndex);*/
 			/* Time to go home! TGIF! */
-			if (my._done) {
+			if (GetMV(my, CISDone)) {
 				Printf1("Airline check-in staff %d of airline %d is closing the counter\n",
 					sizeof("Airline check-in staff %d of airline %d is closing the counter\n"),
 					concat2Num(_myIndex, _myAirline));
-/*				Acquire(my._lock);*/
-				Release(myAirline._lock);
-/*				Wait(my._lock, my._commCV); *//* Wait forever, basically */
-/*				Release(my._lock); *//* Never reaches here, but whatever... */
+/*				Acquire(GetMV(my, CISLock));*/
+				Release(GetMV(myAirline, AirlineLock));
+/*				Wait(GetMV(my, CISLock), GetMV(my, CISCommCV)); *//* Wait forever, basically */
+/*				Release(GetMV(my, CISLock)); *//* Never reaches here, but whatever... */
 				break;
 			}
 			myAirline._numOnBreakCIS--;
 		}
 		/* Start helping a passenger */
-		my._state = BUSY;
-		Acquire(myAirline._cisLineLock);
-		Acquire(myAirline._execLineLock);
-		if (queue_size(&myAirline._execQueue) > 0) {
-			my._currentPassenger = queue_pop(&myAirline._execQueue);
+		SetMV(my, CISState, BUSY);
+		Acquire(GetMV(myAirline, AirlineCISLineLock));
+		Acquire(GetMV(myAirline, AirlineExecLineLock));
+		if (queue_size(GetMV(myAirline, AirlineExecQueue)) > 0) {
+			SetMV(my, CISCurrentPassenger, queue_pop(GetMV(myAirline, AirlineExecQueue)));
 			passenger._cisID = _myIndex;
 			Printf1("Airline check-in staff %d of airline %d serves an executive class passenger and economy line length = %d\n",
 				sizeof("Airline check-in staff %d of airline %d serves an executive class passenger and economy line length = %d\n"),
-				concat3Num(_myIndex, _myAirline, my._lineSize));
-			Signal(myAirline._execLineLock, myAirline._execLineCV); /* Signal Passenger */
-		} else if (my._lineSize > 0) {
+				concat3Num(_myIndex, _myAirline, GetMV(my, CISLineSize)));
+			Signal(GetMV(myAirline, AirlineExecLineLock), GetMV(myAirline, AirlineExecLineCV)); /* Signal Passenger */ 
+		} else if (GetMV(my, CISLineSize) > 0) {
 			Printf1("Airline check-in staff %d of airline %d serves an economy class passenger and executive class line length = %d\n",
 				sizeof("Airline check-in staff %d of airline %d serves an economy class passenger and executive class line length = %d\n"),
-				concat3Num(_myIndex, _myAirline, queue_size(&myAirline._execQueue)));
-			Signal(myAirline._cisLineLock, my._lineCV);
+				concat3Num(_myIndex, _myAirline, queue_size(GetMV(myAirline, AirlineExecQueue))));
+			Signal(GetMV(myAirline, AirlineCISLineLock), GetMV(my, CISLine));
 /*Printf1("Cis %d of airline %d wakes up passenger\n", sizeof("Cis %d of airline %d wakes up passenger\n"), _myIndex*1000+_myAirline);*/
 		}
 		/* Interact with Passenger */
-		Acquire(my._lock);
-		Release(myAirline._cisLineLock);
-		Release(myAirline._execLineLock);
-		Release(myAirline._lock);
-		if (my._lineSize > 0 || my._currentPassenger != -1) {
+		Acquire(GetMV(my, CISLock));
+		Release(GetMV(myAirline, AirlineCISLineLock));
+		Release(GetMV(myAirline, AirlineExecLineLock));
+		Release(GetMV(myAirline, AirlineLock));
+		if (GetMV(my, CISLineSize) > 0 || GetMV(my, CISCurrentPassenger) != -1) {
 /*Printf1("Cis %d of airline %d goes to sleep\n", sizeof("Cis %d of airline %d goes to sleep\n"), _myIndex*1000+_myAirline);*/
-			Wait(my._lock, my._commCV); 
+			Wait(GetMV(my, CISLock), GetMV(my, CISCommCV)); 
 		} /* Otherwise, Manager woke you up for no reason */
-		if (my._currentPassenger != -1) {
+		if (GetMV(my, CISCurrentPassenger) != -1) {
 			int i;
 			/* Assign seat number */
-			Acquire(myAirline._lock);
-			passenger._ticket._seat = myAirline._numCheckedinPassengers;
-			myAirline._numCheckedinPassengers++;
-			Release(myAirline._lock);
+			Acquire(GetMV(myAirline, AirlineLock));
+			SetMV(passenger, PassengerTicketSeat, GetMV(myAirline, AirlineNumCheckedinPassengers));
+			IncrementMV(myAirline, AirlineNumCheckedinPassengers);
+			Release(GetMV(myAirline, AirlineLock));
 			/* Deal with baggage */
 			Acquire(ConveyorLock);
 			for (i = 0; i < passenger._numBaggages; ++i) {
@@ -105,7 +105,7 @@ void startCheckInStaff() {
 					sizeof("Airline check-in staff %d of airline %d dropped bags to the conveyor system \n"),
 					concat2Num(_myIndex, _myAirline));
 /*				myAirline._numExpectedBaggages++;*/
-				my._weightCount += bag._weight;
+				GetMV(my, CISWeightCount) += bag._weight;
 				#undef bag
 /*				#undef bIndex*/
 			}
@@ -114,19 +114,19 @@ void startCheckInStaff() {
 			if (passenger._ticket._executive) {
 				Printf2("Airline check-in staff %d of airline %d informs executive class passenger %d to board at gate %d\n",
 					sizeof("Airline check-in staff %d of airline %d informs executive class passenger %d to board at gate %d\n"),
-/*					concat3Num(_myAirline, my._currentPassenger, _myAirline), _myIndex);*/
-					concat3Num(_myIndex, _myAirline, my._currentPassenger), _myAirline);
+/*					concat3Num(_myAirline, GetMV(my, CISCurrentPassenger), _myAirline), _myIndex);*/
+					concat3Num(_myIndex, _myAirline, GetMV(my, CISCurrentPassenger)), _myAirline);
 			} else {
 				Printf2("Airline check-in staff %d of airline %d informs economy class passenger %d to board at gate %d\n",
 					sizeof("Airline check-in staff %d of airline %d informs economy class passenger %d to board at gate %d\n"),
-/*					concat3Num(_myAirline, my._currentPassenger, _myAirline), _myIndex);*/
-					concat3Num(_myIndex, _myAirline, my._currentPassenger), _myAirline);
+/*					concat3Num(_myAirline, GetMV(my, CISCurrentPassenger), _myAirline), _myIndex);*/
+					concat3Num(_myIndex, _myAirline, GetMV(my, CISCurrentPassenger)), _myAirline);
 			}
-			Signal(my._lock, my._commCV); 
-			Wait(my._lock, my._commCV); 
+			Signal(GetMV(my, CISLock), GetMV(my, CISCommCV)); 
+			Wait(GetMV(my, CISLock), GetMV(my, CISCommCV)); 
 		}
-		my._currentPassenger = -1;
-		Release(my._lock);
+		SetMV(my, CISCurrentPassenger, -1);
+		Release(GetMV(my, CISLock));
 	} /* end while */
 	Exit(0);
 #undef passenger
