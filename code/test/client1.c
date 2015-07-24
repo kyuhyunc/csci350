269147b5f563e6fc1987f682ab9949d33d4ccaaf
Greg Berger <grberger@usc.edu>
2015-07-24 00:34:54 -0700

    Added doInit to client1

--- code/test/client1.c
+++ code/test/client1.c
@@ -1,11 +1,16 @@
 #include "create.h"
 
 extern void doCreates();
+extern void doInitialize();
 
 int main(){
 	int i;
 	char* test;
 	char buf[30];
+
+	/* Client 1 must be run first to ensure values are initialized */
+	doCreates();
+	doInitialize();
 
 	for (i = 0; i < NUM_PASSENGERS; ++i) {
 		Exec("../test/passenger", sizeof("../test/passenger"));
