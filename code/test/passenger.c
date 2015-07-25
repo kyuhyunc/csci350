749ce59aafbb7adcfcfb62fbd4a221aa1d20d8cc
Greg Berger <grberger@usc.edu>
2015-07-24 22:19:54 -0700

    Converted passenger CIS interaction

--- code/test/passenger.c
+++ code/test/passenger.c
@@ -26,6 +26,8 @@ void startPassenger() {
 	int _myMV;
 	int _minLineSize;
 	int _liaison;
+	int _myAirline;
+	int _myCIS;
 	int temp0, temp1, temp2, temp3;
 
     Acquire(GlobalDataLock);
@@ -139,6 +141,58 @@ void startPassenger() {
 		GetMV(_liaison, LiaisonCommCV) ); /* Signal Liaison */
 	Release(GetMV(_liaison, LiaisonLock));
 	/* end Liaison Interaction */
+
+	/*
+		Check-in Staff Interaction
+	*/
+	_myAirline = GetMV(airlines, GetMV(_myMV, PassTicketAirline));
+	if (GetMV(_myMV, PassTicketExecutive)) {
+		Acquire(GetMV(_myAirline, AirlineExecLineLock));
+		queue_insert( GetMV(_myAirline, AirlineExecQueue), _myMV );
+		Printf1("Passenger %d of Airline %d is waiting in the executive class line\n", 
+			sizeof("Passenger %d of Airline %d is waiting in the executive class line\n"),
+			concat2Num(_myIndex, GetMV(_myMV, PassTicketAirline)));
+/*		Wait(myAirline._execLineLock, myAirline._execLineLock); *//* Wait on CIS */
+		Wait(GetMV(_myAirline, AirlineExecLineLock), GetMV(_myAirline, AirlineExecLineCV)); /* Wait on CIS */
+	} else { /* Economy */
+		Acquire(GetMV(_myAirline, AirlineCISLineLock));
+		/* Find shortest line */
+		_minLineSize = GetMV(GetMV(GetMV(_myAirline, AirlineCIS), 0), CISLineSize); /* declare at top of startPassenger */
+		_myCIS = GetMV(GetMV(_myAirline, AirlineCIS), 0);
+		for (i = 0; i < NUM_CIS_PER_AIRLINE; ++i) {
+			int nextSize = GetMV(GetMV(GetMV(_myAirline, AirlineCIS), i), CISLineSize);
+			if ( nextSize < _minLineSize ) {
+				_minLineSize = nextSize;
+				my._cisID = i;
+				_myCIS = GetMV(GetMV(_myAirline, AirlineCIS), i);
+			}
+		}
+		Printf2("Passenger %d of Airline %d chose Airline Check-In staff %d with a line length %d\n", 
+			sizeof("Passenger %d of Airline %d chose Airline Check-In staff %d with a line length %d\n"),
+			concat3Num(_myIndex, GetMV(_myMV, PassTicketAirline), _myCIS), _minLineSize);
+			incrementMV(_myCIS, CISLineSize);
+		Wait( GetMV(_myAirline, AirlineCISLineLock), GetMV( _myCIS, CISLineCV ) );
+	}
+	Acquire(myCIS._lock);
+	Acquire( GetMV(_myCIS, CISLock) );
+	if ( GetMV(_myMV, PassTicketExecutive) ) {
+		Release( _myAirline, AirlineExecLineLock );
+	} else {
+		decrementMV( _myCIS, CISLineSize );
+		Release( _myAirline, AirlineCISLineLock );
+	}
+	/* Give baggage to CIS */
+	incrementMV( _myCIS, CISPassCount );
+	SetMV( _myCIS, CISBagCount, GetMV(_myCIS, CISBagCount) + GetMV(_myMV, PassNumBaggages) );
+	SetMV( _myCIS, CISCurrentPassenger, _myMV );
+	Signal( GetMV(_myCIS, CISLock), GetMV(_myCIS, CISCommCV) ); /* Signal CIS */
+	Wait( GetMV(_myCIS, CISLock), GetMV(_myCIS, CISCommCV) ); /* Wait CIS */
+	Printf1("Passenger %d of Airline %d was informed to board at gate %d\n",
+		sizeof("Passenger %d of Airline %d was informed to board at gate %d\n"),
+		concat3Num(_myIndex, GetMV(_myMV, PassTicketAirline), GetMV(_myMV, PassTicketAirline)));
+	Signal( GetMV(_myCIS, CISLock), GetMV(_myCIS, CISCommCV) );
+	Release( GetMV(_myCIS, CISLock) );
+	/* end Check-in Staff Interaction */
 
 	Exit(0);
 }
