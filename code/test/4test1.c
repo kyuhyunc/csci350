56246fbf751767199b8a2408cff81836fccabe71
Taegyum Kim <taegyumk@usc.edu>
2015-07-24 20:52:48 -0700

    update c test file

--- code/test/4test1.c
+++ code/test/4test1.c
@@ -5,6 +5,7 @@ This should be run before running test 2
 
 #include "syscall.h"
 
+void useMV();
 
 void createWait() {
 	int firstLock;
@@ -15,13 +16,31 @@ void createWait() {
 
 	Acquire(firstLock);
 	Wait(firstLock, firstCV);
-	Printf0("after wait in test 1", sizeof("after wait in test 1"));
+	Printf0("after wait in test 1\n", sizeof("after wait in test 1\n"));
 	Release(firstLock);
-	Printf0("after release in test 1", sizeof("after release in test 1"));
+	Printf0("after release in test 1\n", sizeof("after release in test 1\n"));
+
+
+	useMV();
 
 	Exit(0);
 }
 
+void useMV() {
+	int firstMV, secondMV;
+	int gettingMV1, gettingMV2;
+	Printf0("Getting MV!\n", sizeof("GettingMV!\n"));
+	firstMV = CreateMV("firstMV", sizeof("firstMV"), 10);
+	secondMV = CreateMV("secondMV", sizeof("secondMV"), 9);
+
+	gettingMV1 = GetMV(firstMV, 4);
+	gettingMV2 = GetMV(secondMV, 2);
+
+	Printf1(
+	"Checking MV %d %d %d\n", 
+	sizeof("Checking MV %d %d %d\n"),
+	1000000*gettingMV1 + 1000*gettingMV2 + 21);
+}
 int main() {
 	Fork(createWait, "createWait", sizeof("createWait"));
 	Fork(createWait, "createWait", sizeof("createWait"));
