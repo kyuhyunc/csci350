8699a305d808e64628d81edaad28fada78a02900
Richard Fu <fur@usc.edu>
2015-07-24 22:39:54 -0700

    convert si

--- code/test/screeningofficer.c
+++ code/test/screeningofficer.c
@@ -1,31 +1,13 @@
 #include "create.h"
 
-#define NULL 0
-
-
-typedef int bool;
-enum bool {false, true};
-
-
-/*
-	Utilities	
-*/
-
-int concat3Num(int i, int j, int k) {
-	return 1000000 * i + 1000 * j + k;
-} 
-
-int concat2Num(int i, int j) {
-	return 1000 * i + j;
-}
-
 
 void startScreeningOfficer() {
-	#define mySO ScreeningOfficers[_myIndex]
+	#define mySOMACRO GetMV(screeningOfficers, _myIndex)
 	int _myIndex;
 	/* Claim my screening officer */
 	Acquire(GlobalDataLock);
-    _myIndex = NumActiveScreeningOfficers++;
+    _myIndex = GetMV(NumActiveScreeningOfficers, 0);
+    incrementMV(NumActiveScreeningOfficers, 0);
     Release(GlobalDataLock);
 
     /* Get to work! */
@@ -34,80 +16,80 @@ void startScreeningOfficer() {
 		int shortestLineIndex = -1; /* impossible value */
 		int i; /* iterator */
 		Acquire(OfficersLineLock);
-		if (queue_empty(&OfficersLine)) {
-			mySO._state = ONBREAK;
+		if (queue_empty(officersLine)) {
+			SetMV(mySOMACRO, SOState, ONBREAK);
 /*Printf1("SO %d is going to sleep\n", sizeof("SO %d is going to sleep\n"), _myIndex);*/
 			/* Done? */
-			if (Manager._allSODone) {
+			if (GetMV(manager, ManAllSODone)) {
 				Release(OfficersLineLock);
 				break;
 			}
-			Wait(OfficersLineLock, mySO._commCV); /* Wait for a passenger */
+			Wait(OfficersLineLock, GetMV(mySOMACRO, SOCommCV)); /* Wait for a passenger */
 			/* Done? */
-			if (Manager._allSODone) {
+			if (GetMV(manager, ManAllSODone)) {
 				Release(OfficersLineLock);
 				break;
 			}
 /*Printf1("SO %d is waking up\n", sizeof("SO %d is waking up\n"), _myIndex);*/
 		}
-		if (!queue_empty(&OfficersLine)) {
-			Acquire(mySO._lock);
-			mySO._state = BUSY;
-			mySO._currentPassenger = queue_pop(&OfficersLine);
-			Passengers[mySO._currentPassenger]._officerID = _myIndex;
+		if (!queue_empty(officersLine)) {
+			Acquire(GetMV(mySOMACRO, SOLock));
+			SetMV(mySOMACRO, SOState, BUSY);
+			SetMV(mySOMACRO, SOCurrentPassenger, queue_pop(officersLine));
+			SetMV(GetMV(passengers, GetMV(mySOMACRO, SOCurrentPassenger)), PassOfficerID, _myIndex);
 			Signal(OfficersLineLock, OfficersLineCV); /* Wake up passenger */
 			Release(OfficersLineLock);
-			Wait(mySO._lock, mySO._commCV); /* Wait for passenger to approach */
+			Wait(GetMV(mySOMACRO, SOLock), GetMV(mySOMACRO, SOCommCV)); /* Wait for passenger to approach */
 			/* Generate PASS/FAIL Results */
-			suspicionLevel = (17 * mySO._currentPassenger) % 10; /* PSUEDO rand() */
-			SecurityFailResults[mySO._currentPassenger] = suspicionLevel > 8;
-			if (SecurityFailResults[mySO._currentPassenger]) { /* FAIL */
+			suspicionLevel = (17 * GetMV(mySOMACRO, SOCurrentPassenger)) % 10; /* PSUEDO rand() */
+			SetMV(SecurityFailResults, GetMV(mySOMACRO, SOCurrentPassenger), suspicionLevel > 8);
+
+			if (GetMV(SecurityFailResults, GetMV(mySOMACRO, SOCurrentPassenger))) { /* FAIL */
 				Printf1("Screening officer %d is suspicious of the hand luggage of passenger %d\n",
 					sizeof("Screening officer %d is suspicious of the hand luggage of passenger %d\n"),
-					concat2Num(_myIndex, mySO._currentPassenger));
+					concat2Num(_myIndex, GetMV(mySOMACRO, SOCurrentPassenger)));
 			} else { /* PASS */
 				Printf1("Screening officer %d is not suspicious of the hand luggage of passenger %d\n",
 					sizeof("Screening officer %d is not suspicious of the hand luggage of passenger %d\n"),
-					concat2Num(_myIndex, mySO._currentPassenger));
+					concat2Num(_myIndex, GetMV(mySOMACRO, SOCurrentPassenger)));
 			}
 			/* Find an Available Security Inspector */
 			while (shortestLineIndex == -1) {
 /*Printf1("SO %d is trying to find SI\n", sizeof("SO %d is trying to find SI\n"), _myIndex);*/
-				#define inspector SecurityInspectors[i]
+				#define inspector GetMV(securityInspectors, i)
 				Acquire(InspectorLineLock);	
 /*Printf1("SO %d acquired InspectorLineLock\n", sizeof("SO %d acquired InspectorLineLock\n"), _myIndex);*/
 				for (i = 0; i < NUM_SECURITY_INSPECTORS; ++i) {
-					Acquire(inspector._lock);
+					Acquire(GetMV(inspector, SILock));
 /*Printf1("SO %d is checking SI %d\n", sizeof("SO %d is checking SI %d\n"), concat2Num(_myIndex, i));*/
-					if (inspector._state == AVAIL && inspector._newPassenger == -1) {
+					if (GetMV(inspector, SIState) == AVAIL && GetMV(inspector, SINewPassenger) == -1) {
 						/* Found an inspector! */
-						shortestLineIndex = inspector._id;
-						inspector._newPassenger = mySO._currentPassenger;
-						Release(inspector._lock);
+						shortestLineIndex = GetMV(inspector, SIID);
+						SetMV(inspector, SINewPassenger, GetMV(mySOMACRO, SOCurrentPassenger));
+						Release(GetMV(inspector, SILock));
 						break;
 					}
-					Release(inspector._lock);
+					Release(GetMV(inspector, SILock));
 				}
 				Release(InspectorLineLock);
 				if (shortestLineIndex == -1) {
-Printf1("SO %d is about to Yield\n", sizeof("SO %d is about to Yield\n"), _myIndex);
+					Printf1("SO %d is about to Yield\n", sizeof("SO %d is about to Yield\n"), _myIndex);
 					Yield();
 				}
 				#undef inspector
-/*				Wait(mySO._lock, mySO._commCV);*/
+/*				Wait(GetMV(mySOMACRO, SOLock), GetMV(mySOMACRO, SOCommCV));*/
 			}
 			/* Found an inspetor for the passenger */
-			Passengers[mySO._currentPassenger]._inspectorID = shortestLineIndex;
+			SetMV(GetMV(passengers, GetMV(mySOMACRO, SOCurrentPassenger)), PassInspectorID, shortestLineIndex);
 			Printf1("Screening officer %d directs passenger %d to security inspector %d\n",
 				sizeof("Screening officer %d directs passenger %d to security inspector %d\n"),
-				concat3Num(_myIndex, mySO._currentPassenger, shortestLineIndex));
-			Signal(mySO._lock, mySO._commCV);
-			Release(mySO._lock);
+				concat3Num(_myIndex, GetMV(mySOMACRO, SOCurrentPassenger), shortestLineIndex));
+			Signal(GetMV(mySOMACRO, SOLock), GetMV(mySOMACRO, SOCommCV));
+			Release(GetMV(mySOMACRO, SOLock));
 		} else {
 			Release(OfficersLineLock);
 		}
 	}
-	Exit(0);
 }
 
 int main() {
